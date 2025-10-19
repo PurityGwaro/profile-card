9802356ba926cbfcca90e3bc1a68838b858c350d
PurityGwaro <puritygwaro@gmail.com>
2025-10-19 22:06:58 +0300

    profile card

---
+++ index.js
@@ -0,0 +1,24 @@
+const displayCurrentTime = () =>{
+    const now = new Date()
+    const timeString = now.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  })
+
+document.getElementById('clock').textContent = timeString
+}
+
+displayCurrentTime()
+setInterval(displayCurrentTime, 1000)
+
+const hobbies = ["Playing Chess", "Reading", "Watching Movies", "Evening Walks", "Cooking"]
+
+const dislikes = ["Noise", "Crowded Places", "Drugs", "Slow Service", ""]
+
+const hobbiesList = document.getElementById("hobbies-list")
+hobbiesList.innerHTML = hobbies.map((hobby, index) => `<li class="list-item" data-testid="hobby-${index}">${hobby}</li>`).join("")
+
+const dislikesList = document.getElementById("dislikes-list")
+dislikesList.innerHTML = dislikes.map((dislike, index) => `<li class="list-item" data-testid="dislike-${index}>${dislike}</li>`).join("")
